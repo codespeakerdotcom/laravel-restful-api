6e813f60173c210e0fece4dfa427a352398116b6
Sam <info@codespeaker.com>
2018-10-12 23:08:35 +0800

    Restful tested

---
+++ database/factories/EmployeeFactory.php
@@ -0,0 +1,11 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Employee::class, function (Faker $faker) {
+    return [
+        'emp_name' => $faker->name,
+        'salary'   => $faker->numberBetween($min = 5000, $max = 90000),
+        'job_description'=> $faker->paragraph
+    ];
+});
